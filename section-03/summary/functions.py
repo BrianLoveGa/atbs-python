697ff7a7eb1989f60fbeebae4be76d79f8e9ff9f
Brian Loveless <Brian.loveless1@yahoo.com>
2020-01-11 12:50:59 -0500

    mouse dead break time

---
+++ section-03/summary/functions.py
@@ -0,0 +1,14 @@
+# functions are like mini programs inside the overall program
+# functions main purpose is to get rid of duplicate code
+# def statement defines a function name and input
+# input to functions are arguments - output is return value
+# parameters are variables in between the functions parenthesis
+# return value is specified with return statement
+# every function has return value. if undefined default is return None (like null js)
+
+# from https: // www.pythoncentral.io/python-null-equivalent-none/
+# None is an object - a class. Not a basic type, such as digits, or True and False.
+# In Python 3.x, the type object was changed to new style classes. However the behaviour of None is the same.
+# Because None is an object, we cannot use it to check if a variable exists. It is a value/object, not an operator used to check a condition.
+
+
