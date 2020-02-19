b4330a00493c3745554d4575960059add1076a59
Brian Loveless <Brian.loveless1@yahoo.com>
2020-02-18 20:05:48 -0500

    tic tac toe

---
+++ section-07/dataStructures.py
@@ -0,0 +1,17 @@
+
+import pprint
+# more of this guys cat....
+
+
+dog = {'name':'Roxy', 'color':'black and brown', 'age':11}
+
+allDogs = []
+
+allDogs.append({'name': 'Roxy', 'color': 'black and brown', 'age': 11})
+allDogs.append({'name': 'fido', 'color': 'black ', 'age': 4})
+allDogs.append({'name': 'rover', 'color': 'blonde', 'age': 9})
+allDogs.append({'name': 'fluff', 'color': 'red and brown', 'age': 12})
+allDogs.append({'name': 'milo', 'color': 'gray', 'age': 7})
+
+print(pprint.pformat(allDogs))
+
