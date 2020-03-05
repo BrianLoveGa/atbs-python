5f24ef92b125fec1b0a9ccff6bccfb03fd870501
Brian Loveless <Brian.loveless1@yahoo.com>
2020-03-05 15:54:25 -0500

    mad libs 3 and 4

--- section-10/madlibs.py
+++ section-10/madlibs.py
@@ -29,16 +29,33 @@ import sys
 # hours every day all year making enough _(plural noun)_ to pay
 # for the vacation.
 
+# Have you heard the new band _(ing verb)_ _(adjective)_ _(noun)_ ?
+# They are the next big thing from _(place)_ . Their lead _(insturment)_
+# player from sweden and their guitarist with the _(color)_ hair met in 
+# high school . I won tickets listening to _(noun)_ _(adjective)_ radio
+# do you want to come to the show with me? I'll buy us _(plural noun)_
+# when we get there and we will stop for _(plural noun)_ beforehand.
+# Come on you know it will be a/an _(ing verb)_ time!
+
+
+# Oh boy! That new movie _(city)_ _(plural noun)_ coming out next month,
+# you know about the _(plural noun)_ _(ing verb)_ all night long,
+# it's going to be the _(adjective)_ movie to come out this year!
+# I've heard it's already got _(name)_ award buzz. Well my friend
+# has an awards cut since he's on the _(noun)_ _(adverb)_ commitee of 
+# _(city)_ and he let me borrow it. I'm going to need you to go get us 
+# some _(plural noun)_ and some frosty _(plural noun)_ for snacks.
+# Here's _(number)_ _(animal)_ that oughta cover my share.
 
 print("want to play mad libs?")
 print("enter yes, or ok")
 play = input(">>")
 if play.lower() in ["yes", "sure", "ok"]:
     print("lets play")
-    print("there ar only 2 libs so pick '1' or '2'")
+    print("there are currently 4 'mad libs style games' \n so pick '1' , '2' , '3' , or '4' ..." )
     choice = input(">>")
     if choice in [1, "1", "one"]:
-        print("enter your name")
+        print("Choice one good call please - enter your name")
         name = input(">>")
         print("enter a noun")
         noun1 = input(">>")
@@ -67,9 +84,9 @@ if play.lower() in ["yes", "sure", "ok"]:
         print("On the now infamous " + noun2 + " farm.")
         print("For more we go to " + adj + " " +
               noun3 + " live outside the courthouse...")
-    else:
+    elif choice in[2,"2","two"]:
         # adj1
-        print("Great choice please enter an adjective ")
+        print("Great choice #2 please enter an adjective ")
         adj1 = input(">>")
         # adj2
         print("please enter another adjective")
@@ -145,6 +162,73 @@ if play.lower() in ["yes", "sure", "ok"]:
         print( adj3 +" and because they have to work " + str(numb) + " hours ")
         print("every day all year making enough "+ plNoun3 + " to pay ")
         print("for the vacation.")
+    elif choice in [3,"3","three"]:
+        print("You picked the third story, Awesome! Please enter an ing verb")
+        verbing = input(">>")
+        print("please enter an adjective")
+        adj = input(">>")
+        print("enter a noun")
+        noun = input(">>")
+        print("enter a place")
+        place = input(">>")
+        print("enter a musical insturment")
+        inst = input(">>")
+        print("enter a color")
+        color = input(">>")
+        print("enter a noun")
+        nounA = input(">>")
+        print("enter an adjective")
+        adjA = input(">>")
+        print("enter a plural noun")
+        plural = input(">>")
+        print("enter a plural noun")
+        pluralA = input(">>")
+        print("enter an ing verb")
+        verbingA = input(">>")
+        print("thanks .... here is what you made ...")
+        print("Have you heard the new band" + verbing +" "+ adj +" "+ noun+"?")
+        print("They are the next big thing from "+ place+" . Their lead "+inst )
+        print("player from sweden and their guitarist with the "+color+" hair met in ")
+        print("high school . I won tickets listening to " + nounA +" "+ adjA + " radio ")
+        print("do you want to come to the show with me? I'll buy us" + plural )
+        print("when we get there and we will stop for" + pluralA + " beforehand.")
+        print("Come on you know it will be a/an " + verbingA + " time! ")
+    else:
+        print("the 4rd 'mad lib style' game \n please enter a city")
+        city = input(">>")
+        print("enter a plural noun")
+        plnoun = input(">>")
+        print("enter a plural noun")
+        plnoun2 = input(">>")
+        print("enter an ing verb")
+        ingverb = input(">>")
+        print("enter an adjective")
+        adj = input(">>")
+        print("enter a name")
+        name = input(">>")
+        print("enter a noun")
+        noun = input(">>")
+        print("enter an adverb")
+        adv = input(">>")
+        print("enter a city")
+        city2 = input(">>")
+        print("enter a plural noun")
+        plnoun3 = input(">>")
+        print("enter a plural noun")
+        plnoun4 = input(">>")
+        print("enter a number")
+        numb = input(">>")
+        print("enter a plural animal")
+        plal = input(">>")
+        print("Excellent round 4 done ")
+        print("Oh boy! That new movie " +city+" "+plnoun+" coming out next month,")
+        print("you know about the " +plnoun2 +" "+ ingverb+ " all night long,")
+        print("it's going to be the "+ adj +" movie to come out this year!")
+        print("I've heard it's already got tons of " + name +" award buzz. Well my friend")
+        print("has an 'awards cut' since he's on the "+ noun+ " "+ adv +" commitee of")
+        print(city2 +" and he let me borrow it. I'm going to need you to go get us")
+        print("some "+ plnoun3 +" and some frosty "+plnoun4+" for snacks.")
+        print("Here's "+ str(numb) +" "+ plal +" that oughta cover my share.")
 else:
     print("good bye")
     sys.exit()
